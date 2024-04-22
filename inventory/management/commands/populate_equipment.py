020f2addc19344bf099f9836dd0cf65e552a7735
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-22 16:30:19 +0100

    Changed the script so that it allows to read null values from the excel file.

--- inventory/management/commands/populate_equipment.py
+++ inventory/management/commands/populate_equipment.py
@@ -8,30 +8,37 @@ class Command(BaseCommand):
     help = 'Load equipment data from Excel file using openpyxl'
 
     def handle(self, *args, **options):
-        file_path = r'C:\Users\uthsh\PycharmProjects\djangoProjectGroup\Equiment  Inventory.xlsx'
+        file_path = r'C:\Users\uthsh\PycharmProjects\djangoProjectGroup\media\equipment.xlsx'
         wb = load_workbook(filename=file_path)
-        ws = wb.active
+        ws = wb['MainInventory']
 
         with transaction.atomic():
             for row in ws.iter_rows(min_row=2, values_only=True):
-                if len(row) < 7:  # Check if the row has less than 7 columns
-                    continue  # Skip this row or handle the error as needed
+                if not row or len(row) < 7:  # Ensuring the row exists and has enough columns
+                    continue  # Skip incomplete rows
 
-                location_name = row[4]
+                # Using a default location name if none is provided
+                location_name = row[4] if row[4] else "Default Location"
+
                 location, _ = Location.objects.get_or_create(
                     location_name=location_name,
                     defaults={'location_type': 'Default'}
                 )
 
-                Equipment.objects.update_or_create(
-                    name=row[0],
-                    defaults={
-                        'type': row[1],
-                        'quantity': row[2],
-                        'last_audit': row[3],
-                        'location': location,
-                        'status': row[5] if row[5] else '',
-                        'comments': row[6] if row[6] else ''
-                    }
-                )
+                # Proceed to create/update Equipment only if a name is provided
+                if row[0]:  # Checking if equipment name exists
+                    Equipment.objects.update_or_create(
+                        name=row[0],
+                        defaults={
+                            'type': row[1],
+                            'quantity': row[2],
+                            'last_audit': row[3],
+                            'location': location,
+                            'status': row[5] if row[5] else '',
+                            'comments': row[6] if row[6] else ''
+                        }
+                    )
+                else:
+                    print(f"Skipping equipment creation due to missing name for row: {row}")
+
         print('Data import complete')
