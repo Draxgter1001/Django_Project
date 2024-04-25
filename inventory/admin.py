689fcaf4988311f35d2b45086b74e6e0e429f748
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-25 13:47:32 +0100

    Admin can now view the properties of a reservation from the admin panel

--- inventory/admin.py
+++ inventory/admin.py
@@ -14,8 +14,13 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_approved')
 
 
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'equipment', 'start_date', 'end_date', 'status', 'purpose', 'quantity')
+    list_filter = ('user', 'equipment', 'start_date', 'end_date')
+
+
 # Register models with their respective ModelAdmin classes
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Equipment, EquipmentAdmin)
-admin.site.register(Reservation)
+admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(Location)
